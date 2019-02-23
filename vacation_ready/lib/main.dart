dee127d01828e85517a86188fd4f4374148a7dea
tj-tusharjain <tusharjain.800@gmail.com>
2019-02-23 00:50:27 -0600

    interest page basic layout created

--- vacation_ready/lib/main.dart
+++ vacation_ready/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'interest_create.dart';
 
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
     return new MaterialApp(
-      home: new Interests(),
+      home: new GridListDemo(),
     );
   }
 }
